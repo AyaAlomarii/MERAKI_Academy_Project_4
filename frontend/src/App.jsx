966e880731af1e67077f0ef44e328b5a21a3214a
AyaAlomarii <ayah.aliomari@gmail.com>
2024-01-10 15:37:50 +0300

    START REGISTER page Install Material UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "react/jsx-dev-runtime";
-
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
-
+import Register from './components/Register';
 function App() {
 
 
   return (
     <>
-    
+    <Routes>
+    <Route path="/register" element={<Register />} />
+
+    </Routes>
     </>
   )
 }
