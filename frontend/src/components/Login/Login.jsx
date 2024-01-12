231297f0ba3154f1dcaa362925402b71211fabfa
AyaAlomarii <ayah.aliomari@gmail.com>
2024-01-12 23:42:13 +0300

    some changes

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -14,16 +14,16 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright(props) {
-  return (
+    return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
     </Typography>
-  );
+);
 }
 
 // TODO remove, this demo shouldn't need to reset the theme.
@@ -31,82 +31,82 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
+    const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+        email: data.get('email'),
+        password: data.get('password'),
     });
-  };
+};
 
-  return (
+    return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
+        <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
-          sx={{
+            sx={{
             marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-          }}
+            }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
+            </Avatar>
+            <Typography component="h1" variant="h5">
             Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
             />
             <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
             />
             <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
             />
             <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+                Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
+                <Grid item xs>
                 <Link href="#" variant="body2">
-                  Forgot password?
+                    Forgot password?
                 </Link>
-              </Grid>
-              <Grid item>
+                </Grid>
+                <Grid item>
                 <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                    {"Don't have an account? Sign Up"}
                 </Link>
-              </Grid>
-            </Grid>
-          </Box>
+                </Grid>
+                </Grid>
+            </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
+        </Container>
     </ThemeProvider>
-  );
+);
 }
