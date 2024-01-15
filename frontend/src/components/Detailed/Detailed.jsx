3c35762264417c56393a98ff84c633cbbe3e7e7e
AyaAlomarii <ayah.aliomari@gmail.com>
2024-01-15 12:10:20 +0300

    changes in deatiales page

--- frontend/src/components/Detailed/Detailed.jsx
+++ frontend/src/components/Detailed/Detailed.jsx
@@ -42,13 +42,26 @@ const defaultTheme = createTheme();
 const Detailed = () => {
   const { id } = useParams()
   const [details, setDetails] = useState({})
+  const [plan, setPlan] = useState([])
+  const [schedule, setSchedule] = useState([])
+  const [duties, setDuties] = useState([])
+  const [vol, setVol] = useState({})
+  const [volReq, setVolReq] = useState([])
+
+
   const HandelRender = () => {
     axios
       .get(`http://localhost:5000/initiative/${id}`)
       .then((res) => {
       setDetails(res.data.initiative)
-      
+      setPlan(res.data.initiative.plan)
+      setDuties(res.data.initiative.plan.listOfDuties)
+      setSchedule(res.data.initiative.plan.schedule)
+      setVol(res.data.initiative.volunteerRequirements)
+      //requirementSkills
+       setVolReq(res.data.initiative.volunteerRequirements.requirementSkills)
       })
+    
       .catch((err) => {
       
         console.log("err", err);
@@ -70,14 +83,14 @@ const Detailed = () => {
         <Container sx={{justifyContent:"center"}}  maxWidth="lg">
         <Grid item sx={{justifyItems:"center"}} >
               <Card 
-                sx={{ margin:"auto", pt:5, width:"80%",height: '60%', display: 'flex', flexDirection: 'row' ,alignSelf:"center" }}
+                sx={{ margin:"auto", pt:5, width:"80%",height: '60%', display: 'flex', flexDirection: 'column' ,alignSelf:"center" }}
               >
                 <CardMedia
                   component="div"
                   sx={{
                     // 16:9
-                    width:"70%",
-                    height:"60%",
+                    width:"100%",
+                    height:"100%",
                     pt: '56.25%',
                   }}
                   image={details.img}
@@ -95,6 +108,60 @@ const Detailed = () => {
                   <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
                  Duration: {details.duration}
                   </Typography>
+                  <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                  Target Audience: {plan.targetAudience}
+                  </Typography>
+                  <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                 End Date: {plan.endDate}<br/>
+                 Start Date: {plan.startDate}
+                  </Typography>
+                  <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                 Target Amount: {plan.targetAmount} JOD
+                 </Typography>
+
+                 <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                 Current Amount: {details.currentAmount} JOD
+                  </Typography>
+                  <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                  Volunteer Limit: {details.volunteerLimit} 
+                 </Typography>
+                 <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                 Volunteer Requirements:{/* currentAmount */}
+                 </Typography>
+                 
+                 <Typography component="h2" fontSize= "large" fontWeight="" >
+                 Age Group: {vol.ageGroup} <br/>
+                 Skills: {volReq.map((ele,i)=>{
+                  return <>{ele}</>
+                 })}
+                     
+                  
+                 </Typography>
+
+                  <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                 Duties:{/* currentAmount */}
+                 </Typography>
+                 
+                 <Typography component="h2" fontSize= "large" fontWeight="" >
+                 <ul>
+                  {duties.map((ele,i)=>{
+                    return <li  key={i}>{ele}</li>
+                  })}
+                 </ul>
+                 </Typography>
+                 
+
+                 <Typography component="h2" fontSize= "large" fontWeight="bold" sx={{ pt:2 }}>
+                 schedule:
+                 </Typography>
+                 <Typography component="h2" fontSize= "large" fontWeight="" >
+                 <ul>
+                  {schedule.map((ele,i)=>{
+                    return <li  key={i}>{ele}</li>
+                  })}
+                 </ul>
+                 </Typography>
+                  
                 </CardContent>
               </Card>
             </Grid>
